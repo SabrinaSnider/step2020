38d029b8bb0aa08b1f9ab0317843116c9a7ffcbe
Sabrina Snider <sabrina.snider10@gmail.com>
2020-05-29 13:15:55 -0400

    Rounded purple button edges. Added hard-coded arraylist to servet, and display the data at /data.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,18 +15,40 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> json_data = new ArrayList<String>();
+
+  private void initData() {
+    json_data.add("Wild World");
+    json_data.add("City Folk");
+    json_data.add("New Leaf");
+    json_data.add("New Horizons");
+  }
+
+  private String convertToJson(ArrayList<String> data) {
+    Gson gson = new Gson();
+    return gson.toJson(data);
+  }
+
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Sabrina!</h1>");
+    initData();
+    String json = convertToJson(json_data);
+
+    response.setContentType("application/json");
+    response.getWriter().println(json);
   }
 }
