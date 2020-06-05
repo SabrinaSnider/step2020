43c3befcf79f0cb93d5ce4a379f388fa292594be
Sabrina Snider <sabrina.snider10@gmail.com>
2020-06-05 11:35:08 -0400

    Made PR changes

--- portfolio/src/main/webapp/scripts/animations.js
+++ portfolio/src/main/webapp/scripts/animations.js
@@ -1,102 +1,87 @@
-/* intro section animations */
+// Color constants
+const OFF_WHITE = '#ede7f6';
+const PORTFOLIO_HOVER_BLUE = '#00bcd4';
 
-gsap.to('.layer-1', { y: '-100vh', delay: .5 });
-gsap.to('.layer-2', { y: '-100vh', delay: .7 });
-gsap.to('.layer-3', { y: '-100vh', delay: .9 });
-gsap.to('#overlay', { y: '-100vh', delay: 1.5 });
-gsap.to('.circle', 20, { rotation:"360", ease: Linear.easeNone, repeat: -1 });
+// Intro section animations
 
-setTimeout(() =>
-  type('big-header-main', '>Hi, my name is <span style="color: #9366e3; font-weight: bold;">Sabrina</span>', 1),
-  2000
-)
-setTimeout(() =>
-  type('sub-header-main', '>I like to code', 1),
-  5000
-)
+/* Overlay */
+gsap.to('.layer-left', {y: '-100vh', delay: .5});
+gsap.to('.layer-middle', {y: '-100vh', delay: .7});
+gsap.to('.layer-right', {y: '-100vh', delay: .9});
+gsap.to('#overlay', {y: '-100vh', delay: 1.5});
 
+/* Rotate circles */
+gsap.to('.circle', 20, {rotation:"360", ease: Linear.easeNone, repeat: -1});
+
+/* Fade in portfolio button */
 gsap.fromTo('#portfolio-button', 2,
-  { opacity: 0 }, 
-  { x: '0', opacity: 1, ease:'back.out(1.7)', delay: 7.5 }
-);
+    {opacity: 0}, 
+    {x: '0', opacity: 1, ease:'back.out(1.7)', delay: 5.5});
 
-/* Typewriter effect for title*/
-function type(id, text, i = 0, isTag = false) {
-  if (i === text.length) return;
-  var newText = text.slice(0, ++i); // get text + new char
+// Typewriter effect for title
+const mainHeader = document.getElementById('big-header-main')
+const name = document.getElementById('name')
+const subHeader = document.getElementById('sub-header-main')
 
-  document.getElementById(id).innerHTML = newText;
+ityped.init(mainHeader, { 
+  strings: ['Hi, my name is Sabrina'], 
+  typeSpeed: 70, 
+  startDelay: 2000, 
+  loop: false, 
+  showCursor: false,
+});
 
-  if (newText.slice(-1) === '<') {
-    isTag = true;
-  } else if (newText.slice(-1) === '>') {
-    isTag = false;
-  }
+ityped.init(subHeader, { 
+  strings: ['I like to code'], 
+  typeSpeed: 70, 
+  startDelay: 4000, 
+  loop: false, 
+  showCursor: false,
+});
 
-  if (isTag) return type(id, text, i, true); // if tag, add without delay
-  setTimeout(() => type(id, text, i), 100); // if not a tag, add with a delay
-}
+// Event handlers
 
-/* event handlers */
-
-// Change colors and rotate arrow on hover
-function portfolioBtnHoverOn() {
-  fadeButton('#portfolio-button', '#00bcd4', '#00bcd4', '#ede7f6');
-  gsap.to("#portfolio-arrow", .2, { rotation: "90", ease: Linear.easeNone });
-}
-
-// Change colors and rotate arrow off hover
-function portfolioBtnHoverOff() {
-  fadeButton('#portfolio-button', 'transparent', '#ede7f6', '#ede7f6');
-  gsap.to("#portfolio-arrow", .2, { rotation: "0", ease: Linear.easeNone });
-}
-
-// Fade a button with the given colors and time
-function fadeButton(id, backgroundColor, borderColor, textColor, easeDuration) {
-  gsap.to(id, { 
-    backgroundColor: backgroundColor, 
-    borderColor: borderColor, 
-    color: textColor, 
-    ease:`power1.out(${easeDuration})` 
+/* Change colors and rotate portfolio arrow on hover */
+function portfolioButtonHoverOn() {
+  gsap.to('#portfolio-button', {
+    backgroundColor: PORTFOLIO_HOVER_BLUE, 
+    borderColor: PORTFOLIO_HOVER_BLUE, 
+    color: OFF_WHITE, 
   })
+  gsap.to("#portfolio-arrow", .2, {rotation: "90", ease: Linear.easeNone});
 }
 
-// Change skill dots on/off hover
-function toggleDot(id, years) {
-  const dot = document.getElementById(id);
-  if (dot.classList.contains('flipped')) {
-    dot.classList.remove('flipped');
-    gsap.to("#" + id, 1, { backgroundColor: '#b39ddb' })
-    dot.innerText = id;
-  } else {
-    dot.classList.add('flipped');
-    gsap.to("#" + id, 1, { backgroundColor: '#673ab7' })
-    dot.innerText = years + " years"
-  }
+/* Change colors and rotate back portfolio arrow off hover */
+function portfolioButtonHoverOff() {
+  gsap.to('#portfolio-button', {
+    backgroundColor: 'transparent', 
+    borderColor: OFF_WHITE, 
+    color: OFF_WHITE, 
+  })
+  gsap.to("#portfolio-arrow", .2, {rotation: "0", ease: Linear.easeNone});
 }
 
-/* Scroll to load content */
-const scrollTimeline = new TimelineMax({ onUpdate: () => { scrollTimeline.progress(); } });
+// Scroll to load content
+const scrollTimeline = new TimelineMax({onUpdate: () => {scrollTimeline.progress();}});
 const scrollController = new ScrollMagic.Controller();
 
+/* About me section */
+scrollTimeline.from("#about-me-title", 1, {opacity: 0})
 
-// about me
-const aboutmeAnimation = new TimelineMax();
-
-scrollTimeline.from(".section-header.about-me", 1, { opacity: 0 })
-
-const aboutmeTitleAnimation = new ScrollMagic.Scene({
-  triggerElement: "#about-me-section",
+const aboutmeTitleAnimation = new ScrollMagic.Scene({ 
+  triggerElement: "#about-me-section", // Load title when scrolling into section
   triggerHook: .5,
   duration: "100%",
 })
 
-aboutmeAnimation.from("#selfie-container", 1, { x: '-20vh', opacity: 0 })
-aboutmeAnimation.from("#bio", 1, { x: '20vh', opacity: 0}, "=-.5")
-aboutmeAnimation.from("#selfie", 1, { x: '-20vh', opacity: 0}, "=-1")
+const aboutmeAnimation = new TimelineMax();
+
+aboutmeAnimation.from("#selfie-container", 1, {x: '-20vh', opacity: 0})
+aboutmeAnimation.from("#bio", 1, {x: '20vh', opacity: 0}, "=-.5")
+aboutmeAnimation.from("#selfie", 1, {x: '-20vh', opacity: 0}, "=-1")
 
 const aboutmeSectionAnimation = new ScrollMagic.Scene({
-  triggerElement: ".section-header.about-me",
+  triggerElement: "#about-me-title", // Load the entire content animation after title loads
 })
 
 aboutmeTitleAnimation.setTween(scrollTimeline).addTo(scrollController)
