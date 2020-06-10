48d7abe570ecb6f73b5fff536d1c2e157393a190
Sabrina Snider <sabrina.snider10@gmail.com>
2020-06-09 22:03:43 -0400

    Added animations to workplace placeholders. Edited the animation time of some of the older sections.

--- portfolio/src/main/webapp/scripts/animations.js
+++ portfolio/src/main/webapp/scripts/animations.js
@@ -16,7 +16,7 @@ gsap.to('.circle', 20, {rotation:"360", ease: Linear.easeNone, repeat: -1});
 /* Fade in portfolio button */
 gsap.fromTo('#portfolio-button', 2,
     {opacity: 0}, 
-    {x: '0', opacity: 1, ease:'back.out(1.7)', delay: 5.5});
+    {x: '0', opacity: 1, ease:'back.out(1.7)', delay: 5});
 
 // Typewriter effect for title
 const mainHeader = document.getElementById('big-header-main')
@@ -25,16 +25,16 @@ const subHeader = document.getElementById('sub-header-main')
 
 ityped.init(mainHeader, { 
   strings: ['Hi, my name is Sabrina'], 
-  typeSpeed: 55, 
-  startDelay: 2000, 
+  typeSpeed: 50, 
+  startDelay: 1800, 
   loop: false, 
   showCursor: false,
 });
 
 ityped.init(subHeader, { 
   strings: ['I like to code'], 
-  typeSpeed: 55, 
-  startDelay: 4000, 
+  typeSpeed: 50, 
+  startDelay: 3400, 
   loop: false, 
   showCursor: false,
 });
@@ -69,8 +69,15 @@ const aboutmeAnimation = new TimelineMax();
 
 aboutmeAnimation.from('#about-me-title', .6, {opacity: 0})
 aboutmeAnimation.from("#selfie-container", 1, {x: '-20vh', opacity: 0})
-aboutmeAnimation.from("#bio", 1, {x: '20vh', opacity: 0}, "=-.5")
+aboutmeAnimation.from("#bio-text", 1, {x: '20vh', opacity: 0}, "=-1")
 aboutmeAnimation.from("#selfie", 1, {x: '-20vh', opacity: 0}, "=-1")
+
+aboutmeAnimation.from(".java", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-1")
+aboutmeAnimation.from(".python", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-.9")
+aboutmeAnimation.from(".cpp", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-.9")
+aboutmeAnimation.from(".html", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-.6")
+aboutmeAnimation.from(".css", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-.9")
+aboutmeAnimation.from(".javascript", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-.9")
 
 const aboutmeSectionAnimation = new ScrollMagic.Scene({
   triggerElement: "#about-me-section",
@@ -80,3 +87,24 @@ const aboutmeSectionAnimation = new ScrollMagic.Scene({
 aboutmeSectionAnimation.setTween(aboutmeAnimation).addTo(scrollController)
 
 /* Work Section */
+const workAnimation = new TimelineMax();
+
+// animate workplaces in with a slight delay between each
+aboutmeAnimation.from("#google-logo", 1, {x: '-100vh', ease: "Power2.easeOut"})
+aboutmeAnimation.from("#google-info", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-1")
+
+aboutmeAnimation.from("#uf-info", 1, {x: '-100vh', ease: "Power2.easeOut"}, "=-.8")
+aboutmeAnimation.from("#uf-logo", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-1")
+
+aboutmeAnimation.from("#infotech-logo", 1, {x: '-100vh', ease: "Power2.easeOut"}, "=-.8")
+aboutmeAnimation.from("#infotech-info", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-1")
+
+aboutmeAnimation.from("#medtronic-info", 1, {x: '-100vh', ease: "Power2.easeOut"}, "=-.8")
+aboutmeAnimation.from("#medtronic-logo", 1, {x: '100vh', ease: "Power2.easeOut"}, "=-1")
+
+const workSectionAnimation = new ScrollMagic.Scene({
+  triggerElement: "#work-section",
+  triggerHook: .8,
+})
+
+workSectionAnimation.setTween(workAnimation).addTo(scrollController)
