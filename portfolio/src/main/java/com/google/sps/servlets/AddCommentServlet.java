cf92f15551930e15cabd7b12702afe6a4519dbc2
Sabrina Snider <sabrina.snider10@gmail.com>
2020-06-10 17:13:19 -0400

    Made allCommentImageKeys to not be an Optinal object.

--- portfolio/src/main/java/com/google/sps/servlets/AddCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AddCommentServlet.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -38,34 +38,34 @@ public class AddCommentServlet extends HttpServlet {
     String email = userService.getCurrentUser().getEmail();
     String message = request.getParameter("message");
     long timestamp = System.currentTimeMillis();
-    String image = getUploadedFileUrl(request, "image");
+    Optional<String> image = getUploadedFileUrl(request);
 
     Entity newComment = new Entity("Comment");
 
     newComment.setProperty("email", email);
     newComment.setProperty("timestamp", timestamp);
     newComment.setProperty("message", message);
-    newComment.setProperty("image", image);
+    image.ifPresent(url -> newComment.setProperty("image", url));
 
     DatastoreServiceFactory.getDatastoreService().put(newComment);
   }
 
   /** Returns the URL to the uploaded file or null if the user didn't upload a file. */
-  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+  private Optional<String> getUploadedFileUrl(HttpServletRequest request) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     List<BlobKey> allCommentImageKeys = blobstoreService.getUploads(request).get("image");
 
     // User submitted form without selecting a file (on dev server)
-    if (allCommentImageKeys == null || allCommentImageKeys.isEmpty()) return null;
+    if (allCommentImageKeys == null || allCommentImageKeys.isEmpty()) return Optional.empty();
 
     // Since users can only upload one image per comment, get the first image
     BlobKey imageKey = allCommentImageKeys.get(0);
-
-    // User submitted form without selecting a file (on live server)
     BlobInfo imageInfo = new BlobInfoFactory().loadBlobInfo(imageKey);
+    
+    // User submitted form without selecting a file (on live server)
     if (imageInfo.getSize() == 0) {
       blobstoreService.delete(imageKey);
-      return null;
+      return Optional.empty();
     }
 
     // Use ImagesService to get a URL that points to the uploaded file.
@@ -76,9 +76,9 @@ public class AddCommentServlet extends HttpServlet {
     // the path returned by imagesService (which contains a host)
     try {
       URL url = new URL(imagesService.getServingUrl(options));
-      return url.getPath();
+      return Optional.ofNullable(url.getPath());
     } catch (MalformedURLException e) {
-      return imagesService.getServingUrl(options);
+      return Optional.ofNullable(imagesService.getServingUrl(options));
     }
   }
 }
